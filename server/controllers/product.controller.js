bc9e3ee974f3c7f83b16630d954f2a86b88a796b
Josh Lazar <lazarjosh2@gmail.com>
2024-01-25 13:30:58 -0500

    first commit

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,73 @@
+const Product = require('../models/product.model')
+const Review = require('../models/review.model')
+const User = require('../models/user.model')
+
+
+module.exports = {
+    findAllProducts: async (req, res) => {
+        try {
+            const allProducts = await Product.find();
+            res.status(200).json(allProducts);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred while retrieving products.' });
+        }
+    },
+    createProduct: async (req, res) => {
+        try {
+            console.log(req.body)
+            const image = `http://localhost:8000/uploads/${req.file.filename}`;
+            console.log(image);
+            const newProduct = await Product.create({ ...req.body, image: image });
+            res.status(201).json(newProduct);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred while creating the product.' });
+        }
+    },
+    findFeaturedProducts: async (req, res) => {
+        try {
+            const featuredProducts = await Product.find({ featured: true })
+            res.status(200).json(featuredProducts)
+        }
+        catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred while retreiving featured products.' });
+        }
+    },
+    findOneProduct: async (req, res) => {
+        try {
+            const oneProduct = await Product.findOne({ _id: req.params.id })
+            res.status(200).json(oneProduct)
+        }
+        catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred while retrieving product.' });
+        }
+    },
+    productsByCategory: async (req, res) => {
+        try {
+            const products = await Product.find({ category: req.params.category })
+            res.status(200).json(products)
+        }
+        catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred while retrieving products.' });
+        }
+    },
+    editProduct: async (req, res) => {
+        try {
+            const image = `http://localhost:8000/uploads/${req.file.filename}`;
+            const product = await Product.findOneAndUpdate(
+                { _id: req.params.id },
+                { ...req.body, image: image },
+                { new: true, runValidators: true }
+            )
+            res.status(200).json(product)
+        }
+        catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred while retrieving product.' });
+        }
+    }
+};
---
+++ server/routes/product.routes.js
@@ -0,0 +1,26 @@
+const ProductController = require('../controllers/product.controller')
+const {authenticate} = require('../config/jwt.config')
+
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '../uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}-${Date.now()}.${file.originalname.split('.').pop()}`);
+    },
+});
+
+const upload = multer({ storage });
+
+
+module.exports = app => {
+    app.get('/api/allProducts', ProductController.findAllProducts);
+    app.post('/api/createProduct', authenticate, upload.single('image'), ProductController.createProduct);
+    app.get('/api/featuredProducts', ProductController.findFeaturedProducts);
+    app.get('/api/oneProduct/:id', ProductController.findOneProduct);
+    app.get('/api/oneCategory/:category', ProductController.productsByCategory);
+    app.post('/api/editProduct/:id', authenticate, upload.single('image'), ProductController.editProduct);
+}
